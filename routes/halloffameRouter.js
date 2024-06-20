6f8ec92abc825c164558cf4bd3ed39107fc5e4ba
ZordHD <ms.daniik3@mail.ru>
2024-06-20 19:13:46 +0300

    serv

--- routes/halloffameRouter.js
+++ routes/halloffameRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const halloffameController = require('../controllers/hallOfFameController')
+const halloffameController = require('../controllers/halloffameController')
 
 
 router.post('/', halloffameController.create)
